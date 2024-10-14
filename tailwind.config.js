544cc0a10a8a925b07b3ca05e433d9ea6fedf958
Asaolu Elijah <asaoluelijah7@gmail.com>
2024-10-14 22:26:19 +0100

    added layout to tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			colors: {
-				background: 'var(--background)',
-				primaryGreen: 'var(--primary-green)',
-				mintGreen: 'var(--mint-green)',
-				darkGray: 'var(--dark-gray)',
-				lightGray: 'var(--light-gray)',
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        primaryGreen: "var(--primary-green)",
+        mintGreen: "var(--mint-green)",
+        darkGray: "var(--dark-gray)",
+        lightGray: "var(--light-gray)",
+      },
+    },
+  },
+  plugins: [],
 };
